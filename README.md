412401ace528ce49bba0ee1448e37760b026ade4
Sawai Chungsri <sawaiwai@gmail.com>
2015-10-06 01:05:15 +0700

    Update

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # CodeIgniter-3.0.1-HMVC-HTML-Purifier
 CodeIgniter 3.0.1 + HMVC + HTML Purifier (Clean version)
+
+- CodeIgniter 3.0.1 (Clean version)
+- Modular Extensions - HMVC included (Cr.<a href="https://bitbucket.org/wiredesignz/codeigniter-modular-extensions-hmvc">codeigniter-modular-extensions-hmvc</a>)
+- HTML Purifier included (Cr.<a href="http://htmlpurifier.org/">htmlpurifier</a>)
---
+++ application/modules/test/controllers/Test.php
@@ -0,0 +1,49 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Test extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see http://codeigniter.com/user_guide/general/urls.html
+	 */
+	public function index()
+	{
+		$this->load->helper(array('form', 'url'));
+		$this->load->library('form_validation');
+		$this->form_validation->set_rules('username', 'Username', 'required|callback_username_check');
+		$this->form_validation->set_rules('password', 'Password', 'required');
+		if ($this->form_validation->run() == FALSE)
+		{
+			$this->load->view('test_view');
+		}
+		else
+		{
+			echo "success";
+		}
+	}
+	
+	public function username_check($str)
+	{
+		if ($str == 'test')
+		{
+			$this->form_validation->set_message('username_check', 'The {field} field can not be the word "test"');
+			return FALSE;
+		}
+		else
+		{
+			return TRUE;
+		}
+	}
+}
